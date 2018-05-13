9f7eabd3e70a4fde8ccfdb3fcbf8ebd3feff4dc7
Edmundo Santos <esantos@newsweaver.com>
2018-05-13 15:48:54 +0100

    Update website layout

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="h0">The online home of<br /> Edmundo Santos</h1>
+      <p className="lead">Multidisciplinary designer and coder living in Cork, Ireland.</p>
+    </div>
+  )
+}
+
+export default Hero
