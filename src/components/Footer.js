7a580a167c90cff9ac5d3df0cb3eaa955d95372c
Edmundo Santos <esantos@newsweaver.com>
2018-05-19 08:23:07 +0100

    Improve header and nav panel copy and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,31 @@ export default class Footer extends Component {
   render() {
     return (
       <footer
-        className="footer d-flex flex-column-reverse flex-md-row justify-content-between align-items-center py-4 mt-8 small" role="contentinfo">
+        className="footer d-flex flex-column-reverse flex-sm-row justify-content-between py-4 mt-8 small" role="contentinfo">
         <span>
-          &copy; Edmundo Santos
+          &copy; {(new Date().getFullYear())} Edmundo Santos
         </span>
-        <nav className="mb-2 mb-md-0">
+        <nav className="mb-1 mb-sm-0">
           <a
-            className="text-white"
+            className="d-inline-block pr-1 pl-0 pl-sm-1"
             href="https://dribbble.com/edmundojr">
             Dribbble
           </a>
+          ·
           <a
-            className="text-white ml-2 ml-md-3"
+            className="d-inline-block px-1"
             href="https://gitHub.com/edmundojr">
             GitHub
           </a>
+          ·
           <a
-            className="text-white ml-2 ml-md-3"
+            className="d-inline-block px-1"
             href="https://instagram.com/manfromanotherland">
             Instagram
           </a>
+          ·
           <a
-            className="text-white ml-2 ml-md-3"
+            className="d-inline-block pl-1 pr-1 pr-sm-0"
             href="https://twitter.com/edmundojr_">
             Twitter
           </a>
