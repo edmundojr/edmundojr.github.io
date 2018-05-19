48980c1b1a3bd5a70219faff7fe50ebe38cc6117
Edmundo Santos <hello@edmundojr.com>
2018-05-19 07:43:57 +0100

    Make blog index page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1 className="h0">From the desk of<br /> Edmundo Santos</h1>
-      <p className="lead">Multidisciplinary designer & coder</p>
+    <div className="hero row d-flex align-items-center justify-content-center text-md-center">
+      <div className="col-md-8 mt-5 mt-md-0">
+        <h1 className="display-3">From the desk of Edmundo Santos</h1>
+        <p className="lead">Multidisciplinary designer & coder</p>
+      </div>
     </div>
   )
 }
