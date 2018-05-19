64e848cf7876911804670bc216cf608c0e453e8c
Edmundo Santos <hello@edmundojr.com>
2018-05-19 09:52:55 +0100

    Update hero alignment, add full content on index

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,36 +4,37 @@ import Link from 'gatsby-link'
 export default class Footer extends Component {
   render() {
     return (
-      <footer
-        className="footer d-flex flex-column-reverse flex-sm-row justify-content-between py-4 mt-8 small" role="contentinfo">
-        <span>
-          &copy; {(new Date().getFullYear())} Edmundo Santos
-        </span>
-        <nav className="mb-1 mb-sm-0">
-          <a
-            className="d-inline-block pr-1 pl-0 pl-sm-1"
-            href="https://dribbble.com/edmundojr">
-            Dribbble
-          </a>
-          ·
-          <a
-            className="d-inline-block px-1"
-            href="https://gitHub.com/edmundojr">
-            GitHub
-          </a>
-          ·
-          <a
-            className="d-inline-block px-1"
-            href="https://instagram.com/manfromanotherland">
-            Instagram
-          </a>
-          ·
-          <a
-            className="d-inline-block pl-1 pr-1 pr-sm-0"
-            href="https://twitter.com/edmundojr_">
-            Twitter
-          </a>
-        </nav>
+      <footer className="footer mt-8 font-size-sm" role="contentinfo">
+        <div className="container-fluid d-flex flex-column-reverse flex-md-row justify-content-between py-4">
+          <span>
+            &copy; {(new Date().getFullYear())} Edmundo Santos
+          </span>
+          <nav className="mb-1 mb-md-0">
+            <a
+              className="d-inline-block pr-1 pl-0 pl-md-1"
+              href="https://dribbble.com/edmundojr">
+              Dribbble
+            </a>
+            ·
+            <a
+              className="d-inline-block px-1"
+              href="https://gitHub.com/edmundojr">
+              GitHub
+            </a>
+            ·
+            <a
+              className="d-inline-block px-1"
+              href="https://instagram.com/manfromanotherland">
+              Instagram
+            </a>
+            ·
+            <a
+              className="d-inline-block pl-1 pr-1 pr-md-0"
+              href="https://twitter.com/edmundojr_">
+              Twitter
+            </a>
+          </nav>
+        </div>
       </footer>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
-class HomeIndex extends React.Component {
+export default class HomeIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
     return (
-      <main className="row">
+      <main className="container-fluid">
         <Helmet title={siteTitle + ' × Articles'} />
         {posts.map(({ node }) => {
           const title = get(node, 'frontmatter.title') || node.fields.slug
@@ -17,22 +17,21 @@ class HomeIndex extends React.Component {
             return `<span>${a}</span>`
           })
           return (
-            <article key={node.fields.slug} className="col post mb-5">
-              <div className="row align-items-center">
+            <article key={node.fields.slug} className="post mb-5">
+              <header className="row align-items-center">
                 <div className="order-first d-none d-md-block col-md-1 col-lg-1 offset-lg-1">
-                  <div className="bg-white" style={{paddingTop: '4px'}}></div>
+                  <div className="bg-white" style={{ paddingTop: '4px' }}></div>
                 </div>
-                <header className="order-2 col-sm-8 col-md-7">
-                  <h1>
-                    <Link to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h1>
-                </header>
-              </div>
+                <h1 className="order-last col-sm-8 col-md-7">
+                  <Link to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h1>
+              </header>
               <div className="row">
-                <div className="order-last order-sm-3 col-sm-8 col-md-7 offset-md-1 offset-lg-2">
-                  <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                <div
+                  className="order-last order-sm-3 col-sm-8 col-md-7 offset-md-1 offset-lg-2"
+                  dangerouslySetInnerHTML={{ __html: node.html }}>
                 </div>
                 <aside className="post__meta order-3 order-sm-last col-sm-4 col-md-3 text-sm-center text-uppercase">
                   <time className="post__date d-inline-block mb-1 mb-sm-0" dangerouslySetInnerHTML={{ __html: postDate }} />
@@ -46,8 +45,6 @@ class HomeIndex extends React.Component {
   }
 }
 
-export default HomeIndex
-
 export const pageQuery = graphql`
   query BlogQuery {
     site {
@@ -58,10 +55,10 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          excerpt(pruneLength: 480)
           fields {
             slug
           }
+          html
           frontmatter {
             date(formatString: "DD MMM YYYY")
             title
