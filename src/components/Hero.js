ebd5501f9c2c2a79aafe59e121a94590a63d25c1
Edmundo Santos <esantos@newsweaver.com>
2018-05-20 10:06:55 +0100

    Improve styles for small screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="hero row align-items-center">
         <div className="col-sm-8 col-md-7 offset-md-1 offset-lg-2">
           <h1 className="display-4">
-            <span className="d-md-block">From the desk of </span>
+            <span className="d-xl-block">From the desk of </span>
             Edmundo Santos
           </h1>
           <p className="lead">Multidisciplinary designer & coder</p>
