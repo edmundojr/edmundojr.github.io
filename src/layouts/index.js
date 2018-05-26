5315fbaa486fde7e2840aa0a5e646b3d17bb7243
Edmundo Santos <hello@edmundojr.com>
2018-05-26 17:57:12 +0100

    SEO: add meta description

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,6 +16,7 @@ class Template extends React.Component {
         <Helmet>
           <html lang="en" />
           <meta name="theme-color" content="#A6B2BB" />
+          <meta name="description" content="Edmundo Santos is a multidisciplinary designer, coder, photo enthusiast, book lover, and occasional wanderer living in Ireland." />
           <link rel="icon" type="image/png" href="/favicon@16px.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon@32px.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/favicon@48px.png" sizes="48x48" />
