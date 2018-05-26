2988ceb2e4c73ca2f32e00dff30ae436d809299f
Edmundo Santos <esantos@newsweaver.com>
2018-05-26 18:07:14 +0100

    Progressive web app: add manifest.json and icon for web app

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,10 +17,12 @@ class Template extends React.Component {
           <html lang="en" />
           <meta name="theme-color" content="#A6B2BB" />
           <meta name="description" content="Edmundo Santos is a multidisciplinary designer, coder, photo enthusiast, book lover, and occasional wanderer living in Ireland." />
-          <link rel="icon" type="image/png" href="/favicon@16px.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="/favicon@32px.png" sizes="32x32" />
-          <link rel="icon" type="image/png" href="/favicon@48px.png" sizes="48x48" />
-          <link rel="icon" type="image/png" href="/favicon@96px.png" sizes="96x96" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192" />
         </Helmet> 
       	<Header />
         <Hero />
