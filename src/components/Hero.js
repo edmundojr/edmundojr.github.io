a35a4c7ec2ed738810d29b04d8f26161937ae88f
Edmundo Santos <esantos@newsweaver.com>
2018-05-26 19:09:40 +0100

    Style small hero variation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,11 @@ const Hero = () => {
     <div className="container-fluid">
       <div className="hero row align-items-center">
         <div className="col-sm-8 col-md-7 offset-md-1 offset-lg-2">
-          <h1 className="display-4">
-            <span className="d-xl-block">From the desk of </span>
+          <h1 className="hero__title display-4">
+            From the desk of 
             Edmundo Santos
           </h1>
-          <p className="lead">Multidisciplinary designer & coder</p>
+          <p className="hero__description lead mb-0">Multidisciplinary designer & coder</p>
         </div>
       </div>
     </div>
