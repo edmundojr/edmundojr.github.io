79615f9737cff7774dc58c68bc91035c5eb96527
Edmundo Santos <hello@edmundojr.com>
2018-05-28 07:13:28 +0100

    Design: make hero smaller on article pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="container-fluid">
-      <div className="hero row align-items-center">
+      <div className={"hero row align-items-center mt-8 my-md-0 " + (location.pathname.startsWith('/blog') ? 'hero--small' : '')}>
         <div className="col-sm-9 col-md-7 offset-sm-1 offset-lg-2">
-          <h1 className="hero__title mb-1">
+          <h1 className="hero__title mb-0">
             From the desk of 
             Edmundo Santos
           </h1>
-          <p className="hero__description lead mb-0">Multidisciplinary designer & coder</p>
+          <p className="hero__description lead text-nowrap mb-0">Multidisciplinary designer & coder</p>
         </div>
       </div>
     </div>
