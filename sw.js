324e9a96ed723571262e3d9e2b70e8bf1c7d834a
Edmundo Santos <esantos@newsweaver.com>
2018-05-28 07:47:22 +0100

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1a65a3594b7d89eb7a29.js","c10276264c7f48cd81f276ca1e6046d4"],["/app-2043498ae98591b54dff.js","05027f92145c64aca3eccbc5802a3c43"],["/app-33e3cf738b06b005dac1.js","aa458ed882306635cf8cf6ff1168855a"],["/app-38cb3e9d4cf7c429b44a.js","7d48d21da747fa6e6b7fab92fa6fee04"],["/app-7c822a344ca1c812cb82.js","eeb69c99d9cf3d1217a21a9b8d25c261"],["/commons-b473c4ddef8d57bfbb89.js","6d1daf7f32fa1b219478d2e4d4ee22cc"],["/index.html","bdd97d2b6c4580035c7fffb6bfbf316b"],["/manifest.json","52506922e5d8bf489cbc84fd4196f1ef"],["/offline-plugin-app-shell-fallback/index.html","c361b1cf582ebd1ac27f6da9cee407ec"]];
+var precacheConfig = [["/app-1a65a3594b7d89eb7a29.js","c10276264c7f48cd81f276ca1e6046d4"],["/app-2043498ae98591b54dff.js","05027f92145c64aca3eccbc5802a3c43"],["/app-33e3cf738b06b005dac1.js","61e3d092dbf7805dcc44921766e07876"],["/app-38cb3e9d4cf7c429b44a.js","7d48d21da747fa6e6b7fab92fa6fee04"],["/app-7c822a344ca1c812cb82.js","eeb69c99d9cf3d1217a21a9b8d25c261"],["/commons-b473c4ddef8d57bfbb89.js","3facbc2a43d051c1c77ac96699885c69"],["/index.html","06c3badc71d656f403681acc74e0199a"],["/manifest.json","52506922e5d8bf489cbc84fd4196f1ef"],["/offline-plugin-app-shell-fallback/index.html","0b6c6f92b974d92b060c1483af94abb6"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
