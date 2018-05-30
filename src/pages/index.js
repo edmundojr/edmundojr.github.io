0c98c902f208ea369a97424ff0a19a31eada3f12
Edmundo Santos <hello@edmundojr.com>
2018-05-30 05:55:46 +0100

    Refactor both index and blog post pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import get from 'lodash/get'
 
 export default class HomeIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
     return (
       <main className="container-fluid">
         <Helmet title={siteTitle + ' × Articles'} />
         {posts.map(({ node }) => {
-          const postTitle = get(node, 'frontmatter.title')
-          const postLink = get(node, 'fields.slug')
+          const postTitle = node.frontmatter.title
+          const postLink = node.fields.slug
           const postDate = node.frontmatter.date.replace(/\S+/g, function(a) {
             return `<span>${a}</span>`
           })
+          const postDate2 = node.frontmatter.date
+          const postTags = node.frontmatter.tags
           return (
-            <article key={postLink} className="post">
+            <article key={`${postLink}`} className="post">
               <h1 className="post__title">
-                <Link to={postLink} className="post__title__link row align-items-center">
+                <Link to={`${postLink}`} className="post__title__link row align-items-center">
                   <span className="post__title__detail col-1 offset-lg-1 order-last order-sm-first"></span>
                   <span className="col-11 col-md-7 order-first order-sm-last">
                     {postTitle}
@@ -31,16 +32,16 @@ export default class HomeIndex extends React.Component {
               <div className="row align-items-start">
                 <div
                   className="col-sm-11 col-md-7 offset-sm-1 offset-lg-2 order-last order-md-first"
-                  dangerouslySetInnerHTML={{ __html: node.html }}>
-                </div>
+                  dangerouslySetInnerHTML={{ __html: node.html }} />
                 <aside className="post__meta col-sm-11 col-md-3 text-md-center offset-sm-1 offset-md-0 order-first order-md-last position-sticky mb-2">
-                  <time className="post__date d-inline-block" dangerouslySetInnerHTML={{ __html: postDate }} />
+                  <time
+                    className="post__date d-inline-block"
+                    dangerouslySetInnerHTML={{ __html: postDate }} />
                   <ul className="post__tags">
-                    {get(node, 'frontmatter.tags').map((tag, index) => {
+                    {postTags.map((tag, index) => {
                       return (
-                        <li key={index}>
+                        <li key={`${index}`}>
                           {tag}
-                          {/* <Link to={`/tags/${tag}`} className="post__tags__tag">{tag}</Link> */}
                         </li>
                       )
                     })}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,48 +6,45 @@ import get from 'lodash/get'
 class BlogPostTemplate extends React.Component {
   render() {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
-
     const post = this.props.data.markdownRemark
     const postTitle = post.frontmatter.title
     const postLink = post.frontmatter.slug
-    const postDate = post.frontmatter.date
+    const postDate = post.frontmatter.date.replace(/\S+/g, function (a) {
+      return `<span>${a}</span>`
+    })
     const postTags = post.frontmatter.tags
-
-    const postContent = `
-      <div class="row align-items-start">
-        <div class="col-sm-11 col-md-7 offset-sm-1 offset-lg-2 order-last order-md-first">
-          ${post.html}
-        </div>
-        <aside class="post__meta col-sm-11 col-md-3 text-md-center offset-sm-1 offset-md-0 order-first order-md-last position-sticky mb-2">
-          <time class="post__date d-inline-block mb-sm-0">
-            ${postDate.replace(/\S+/g, function (a) {
-              return `<span>${a}</span>`
-            })}
-          </time>
-          <ul class="post__tags">
-            ${postTags.map((tag, index) => {
-              return `
-                <li key=${index}>${tag}</li>
-              `
-            }).join("")}
-          </ul>
-        </aside>
-      </div>
-    `
 
     return (
       <main className="container-fluid">
         <Helmet title={`${postTitle} × ${siteTitle}`} />
         <article className="post">
           <h1 className="post__title">
-            <Link to={postLink} className="post__title__link row align-items-center">
+            <Link to={`${postLink}`} className="post__title__link row align-items-center">
               <span className="post__title__detail col-1 offset-lg-1 order-last order-sm-first"></span>
               <span className="col-11 col-md-7 order-first order-sm-last">
                 {postTitle}
               </span>
             </Link>
           </h1>
-          <div dangerouslySetInnerHTML={{ __html: postContent }} />
+          <div className="row align-items-start">
+            <div
+              className="col-sm-11 col-md-7 offset-sm-1 offset-lg-2 order-last order-md-first" 
+              dangerouslySetInnerHTML={{ __html: post.html }} />
+            <aside className="post__meta col-sm-11 col-md-3 text-md-center offset-sm-1 offset-md-0 order-first order-md-last position-sticky mb-2">
+              <time
+                className="post__date d-inline-block mb-sm-0"
+                dangerouslySetInnerHTML={{ __html: postDate }} />
+              <ul className="post__tags">
+                {postTags.map((tag, index) => {
+                  return (
+                    <li key={`${index}`}>
+                      {tag}
+                    </li>
+                  )
+                })}
+              </ul>
+            </aside>
+          </div>
         </article>
       </main>
     )
