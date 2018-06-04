51767bf729aa19bfe7f54e78809efce284bfbb98
Edmundo Santos <hello@edmundojr.com>
2018-06-04 11:18:52 +0100

    Refactor some components

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,52 +3,46 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import get from 'lodash/get'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
-    const post = get(this.props, 'data.markdownRemark')
-    const postTitle = post.frontmatter.title 
-    const postLink = post.frontmatter.slug
-    const postDate = post.frontmatter.date.replace(/\S+/g, function (a) {
-      return `<span>${a}</span>`
-    })
-    const postTags = post.frontmatter.tags
+const BlogPostTemplate = (props) => {
+  const siteTitle = get(props, 'data.site.siteMetadata.title')
+  const post = get(props, 'data.markdownRemark')
+  const postTitle = post.frontmatter.title 
+  const postLink = post.frontmatter.slug
+  const postDate = post.frontmatter.date.replace(/(\S+)/g, '<span>$1</span>')
+  const postTags = post.frontmatter.tags
 
-    return (
-      <main className="container-fluid">
-        <Helmet title={`${postTitle} × ${siteTitle}`} />
-        <article className="post">
-          <h1 className="post__title">
-            <div className="post__title__link row align-items-center">
-              <span className="post__title__detail col-1 offset-lg-1 order-last order-sm-first"></span>
-              <span className="col-11 col-md-7 order-first order-sm-last">
-                {postTitle}
-              </span>
-            </div>
-          </h1>
-          <div className="row align-items-start">
-            <div
-              className="col-sm-11 col-md-7 offset-sm-1 offset-lg-2 order-last order-md-first" 
-              dangerouslySetInnerHTML={{ __html: post.html }} />
-            <aside className="post__meta col-sm-11 col-md-3 text-md-center offset-sm-1 offset-md-0 order-first order-md-last position-sticky mb-2">
-              <time
-                className="post__date d-inline-block mb-sm-0"
-                dangerouslySetInnerHTML={{ __html: postDate }} />
-              <ul className="post__tags">
-                {postTags.map((tag, index) => {
-                  return (
-                    <li key={`${index}`}>
-                      {tag}
-                    </li>
-                  )
-                })}
-              </ul>
-            </aside>
+  return (
+    <main className="container-fluid">
+      <Helmet title={`${postTitle} × ${siteTitle}`} />
+      <article className="post">
+        <h1 className="post__title">
+          <div className="post__title__link row align-items-center">
+            <span className="post__title__detail col-1 offset-lg-1 order-last order-sm-first"></span>
+            <span className="col-11 col-md-7 order-first order-sm-last">
+              {postTitle}
+            </span>
           </div>
-        </article>
-      </main>
-    )
-  }
+        </h1>
+        <div className="row align-items-start">
+          <div
+            className="col-sm-11 col-md-7 offset-sm-1 offset-lg-2 order-last order-md-first" 
+            dangerouslySetInnerHTML={{ __html: post.html }} />
+          <aside className="post__meta col-sm-11 col-md-3 text-md-center offset-sm-1 offset-md-0 order-first order-md-last position-sticky mb-2">
+            <time
+              className="post__date d-inline-block mb-sm-0"
+              dangerouslySetInnerHTML={{ __html: postDate }} />
+            <ul className="post__tags">
+              {postTags.map((tag, index) => (
+                <li key={`${index}`}>
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </aside>
+        </div>
+      </article>
+    </main>
+  )
 }
 
 export default BlogPostTemplate
