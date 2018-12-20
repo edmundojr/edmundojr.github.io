daa61ef1d3a9ef555147eec6f03910c7c56fce1f
Edmundo Santos <hello@edmundojr.com>
2018-12-20 23:00:50 +0000

    Update reading page

--- src/pages/reading.js
+++ src/pages/reading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import get from 'lodash/get'
 import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
+import Layout from '../components/layout'
 import Hero from '../components/Hero'
 import Helmet from 'react-helmet'
 
@@ -15,23 +15,40 @@ export default class LibraryIndex extends React.Component {
       <Layout>
         <Helmet title={siteTitle + ' × Reading'} />
         <Hero
-          type={"small"}
-          title={"From the library of Edmundo Santos"}
-          description={"Designer & Book Nerd"}
+          type={'large'}
+          title={'From the library of Edmundo Santos'}
+          description={'Designer & Book Nerd'}
         />
-        <main className="container-fluid">
-          <div className={"reading"}>
+        <main className={'container-fluid'}>
+          <div className={'reading'}>
             {books.map(({ node }) => {
               const id = node.id
-              const title = node.title_without_series
-              const image = node.image_url
-              const author = node.authors[0].name
+              const bookTitle = node.title_without_series
+              const bookCover = node.large_image_url
+                ? node.large_image_url
+                : node.image_url
+                  ? node.image_url
+                  : node.small_image_url
+              const bookAuthorName = node.authors[0].name
+              const bookAuthorImage = node.authors[0].image_url
               return (
-                <div key={id} className={"book"}>
-                  <img src={image} className="book-cover img-fluid" alt=""/>
-                  <div className={"book-details"}>
-                    {title}<br/>
-                    <div className="book-details-author text-muted">{author}</div>
+                <div key={id} className={'book'}>
+                  <img
+                    src={bookCover}
+                    className={'book-cover img-fluid'}
+                    alt={bookTitle}
+                  />
+                  <div className={'book-details'}>
+                    <h4 className={'book-details-title'}>{bookTitle}</h4>
+                    <div className={'book-details-author'}>
+                      <figure
+                        style={{
+                          backgroundImage: 'url(' + bookAuthorImage + ')',
+                        }}
+                        className={'book-details-author-image img-fluid'}
+                      />
+                      <figcaption>{bookAuthorName}</figcaption>
+                    </div>
                   </div>
                 </div>
               )
@@ -54,19 +71,14 @@ export const pageQuery = graphql`
       edges {
         node {
           id
-          title
           title_without_series
           image_url
-          link
-          num_pages
-          publisher
-          publication_day
-          publication_month
-          publication_year
-          published
+          large_image_url
+          small_image_url
           authors {
             id
             name
+            image_url
           }
         }
       }
