e6eee11fb834167c889397cc61a98bac357cb503
Edmundo Santos <hello@edmundojr.com>
2018-12-26 20:32:42 +0000

    Remove type from Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
-export const Hero = ({ type, title, description }) => (
-  <div className={'container-fluid'}>
-    <div
-      className={
-        'hero row align-items-center justify-content-center text-center mt-8 my-md-0 ' +
-        (type === 'small' ? 'hero--small' : '')
-      }
-    >
-      <div className={'col-sm-9 col-md-6'}>
-        <h1 className={'hero-title mb-0'}>{title}</h1>
-        <p className={'hero-description lead mb-0'}>{description}</p>
-      </div>
+export const Hero = ({ title, description }) => (
+  <div className={'hero container-grid'}>
+    <div className={'hero-content'}>
+      <h1 className={'hero-title mb-0'}>{title}</h1>
+      <p className={'hero-description lead mb-0'}>{description}</p>
     </div>
   </div>
 )
