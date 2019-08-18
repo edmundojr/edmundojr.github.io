c781a079d8c247de93ddb0be9da33c3b1ba29ec1
Edmundo Santos <hello@edmundojr.com>
2019-08-18 11:09:00 +0100

    Setup pose for animations

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import posed, { PoseGroup } from 'react-pose'
 
-export const Hero = React.memo(({ title, description }) => (
-  <div className="hero container-grid">
-    <div className="hero-content">
-      <h1 className="hero-title">{title}</h1>
-      {description && <p className="hero-description lead mb-0">{description}</p>}
-    </div>
-  </div>
-));
+const HeroContent = posed.div({
+  enter: {
+    y: 0,
+    opacity: 1,
+    delay: 100,
+    transition: {
+      y: { type: 'spring', stiffness: 250, damping: 15 },
+      default: { duration: 300 },
+    },
+  },
+  exit: {
+    y: 40,
+    opacity: 0,
+    transition: { duration: 300 },
+  },
+})
+
+export const Hero = React.memo(({ title, description }) => {
+  const [visible, setVisible] = useState(false)
+  useEffect(() => {
+    setVisible(true)
+    return () => setVisible(false)
+  }, [])
+  return (
+    <PoseGroup>
+      {visible && (
+        <HeroContent key={'hero'} className={'hero'}>
+          <h1 className={'hero-title'}>{title}</h1>
+          <p className={'hero-description lead mb-0'}>{description}</p>
+        </HeroContent>
+      )}
+    </PoseGroup>
+  )
+})
+
+Hero.displayName = 'Hero'
