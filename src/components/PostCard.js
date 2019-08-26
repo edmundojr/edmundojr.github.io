8627f89aab061a493578523973f65735827c33bb
Edmundo Santos <esantos@poppulo.com>
2019-08-26 20:20:12 +0100

    Stagger PostCard’s as they animate in

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,65 +1,40 @@
 import { Link } from 'gatsby'
-import React, { useEffect, useState } from 'react'
-import posed, { PoseGroup } from 'react-pose'
+import React from 'react'
+import posed from 'react-pose'
 
-const Card = posed.div({
+const Card = posed(Link)({
   enter: {
-    y: 0,
     opacity: 1,
-    delay: 100,
-    transition: {
-      y: { type: 'spring', stiffness: 250, damping: 15 },
-      default: { duration: 300 },
-    },
+    y: 0,
+    scale: 1,
+    rotateX: 0,
   },
-  exit: {
-    y: 40,
+  init: {
     opacity: 0,
-    transition: { duration: 300 },
+    y: 100,
+    scale: 0.9,
+    rotateX: -25,
   },
   hoverable: true,
   hover: {
-    scale: 1.01,
     y: -5,
-  },
-  init: {
-    scale: 1,
-    y: 0,
-    transition: { type: 'spring', stiffness: 500, damping: 15 },
+    scale: 1.01,
   },
 })
 
-export default React.memo(({ slug, title, date, description, index }) => {
-  const [visible, setVisible] = useState(false)
-  useEffect(() => {
-    setVisible(true)
-    return () => setVisible(false)
-  }, [])
-  return (
-    <PoseGroup>
-      {visible && (
-        <Card
-          key={index}
-          className="post-card grid-item"
-          style={{
-            color: `var(--card-color-${index + 1})`,
-            backgroundColor: `var(--card-bg-${index + 1})`,
-          }}
-        >
-          <Link
-            to={slug}
-            style={{
-              color: `var(--card-color-${index + 1})`,
-              backgroundColor: `var(--card-bg-${index + 1})`,
-            }}
-          >
-            <h1 className="post-card-title">{title}</h1>
-            <time className="post-card-date">{date}</time>
-            <p className="post-card-description">{description}</p>
-            <small className="post-card-read">Read more ›</small>
-          </Link>
-        </Card>
-      )}
-    </PoseGroup>
-  )
-})
+export default React.memo(({ slug, title, date, description, index }) => (
+  <Card
+    to={slug}
+    key={index}
+    className="post-card grid-item"
+    style={{
+      color: `var(--card-color-${index + 1})`,
+      backgroundColor: `var(--card-bg-${index + 1})`,
+    }}
+  >
+    <h1 className="post-card-title">{title}</h1>
+    <time className="post-card-date">{date}</time>
+    <p className="post-card-description">{description}</p>
+    <small className="post-card-read">Read more ›</small>
+  </Card>
+))
